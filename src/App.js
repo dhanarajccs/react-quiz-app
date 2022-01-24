9d6e30d005f609979aaa83bb09b8cd7436cc5ae1
dhanaraj ccs <dhanarajccs@gmail.com>
2022-01-24 20:28:09 +0530

    create moneyListItem

--- src/App.js
+++ src/App.js
@@ -7,17 +7,23 @@ function App() {
       <div className="main">main</div>
       <div className="pyramid">money</div>
         <ul className="moneyList">
-          <li className="moneyListItem">$ 100000</li>
-          <li className="moneyListItem">$ 51200</li>
-          <li className="moneyListItem">$ 25600</li>
-          <li className="moneyListItem">$ 12800</li>
-          <li className="moneyListItem">$ 6400</li>
-          <li className="moneyListItem">$ 3200</li>
-          <li className="moneyListItem">$ 1600</li>
-          <li className="moneyListItem">$ 800</li>
-          <li className="moneyListItem">$ 400</li>
-          <li className="moneyListItem">$ 200</li>
-          <li className="moneyListItem">$ 100</li>
+          <li className="moneyListItem">
+            <span className="moneyListItemNumber">4</span>
+            <span className="moneyListItemAmount">$ 400</span>
+          </li>
+          <li className="moneyListItem">
+            <span className="moneyListItemNumber">4</span>
+            <span className="moneyListItemAmount">$ 400</span>
+          </li>
+          <li className="moneyListItem">
+            <span className="moneyListItemNumber">4</span>
+            <span className="moneyListItemAmount">$ 400</span>
+          </li>
+          <li className="moneyListItem">
+            <span className="moneyListItemNumber">4</span>
+            <span className="moneyListItemAmount">$ 400</span>
+          </li>
+          
         </ul>
 
     </div>
